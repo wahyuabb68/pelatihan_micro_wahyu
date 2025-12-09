a30fb44f9c7cdfda9ae6693150db965a83b13dc3
ajiaja38 <ajicooljazz38@gmail.com>
2025-12-09 10:59:12 +0700

    feat: update minor :zap:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,22 @@
 import Card from "../components/Card/Card"
 import LayoutWrapper from "../layout/LayoutWrapper"
+import products from "../assets/data/products.json"
 
 const Home = () => {
   return (
     <>
       <LayoutWrapper>
         <div className='list-products'>
-          <Card
-            id={1}
-            imgUrl='https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80'
-            category='Fashion'
-            title='Nike Air Zoom Pegasus Merah'
-            price={1500000}
-          />
-          <Card
-            id={2}
-            imgUrl='https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80'
-            category='Fashion'
-            title='Nike Air Hijau'
-            price={2000000}
-          />
-          <Card
-            id={3}
-            imgUrl='https://images.unsplash.com/photo-1633966887768-64f9a867bdba?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80'
-            category='Fashion'
-            title='Kaos Polos Hijau'
-            price={155000}
-          />
-          <Card
-            id={4}
-            imgUrl='https://images.unsplash.com/photo-1651241680016-cc9e407e7dc3?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80'
-            category='Elektronik'
-            title='Macbook Air M1'
-            price={12000000}
-          />
-          <Card
-            id={5}
-            imgUrl='https://images.unsplash.com/photo-1620625515032-6ed0c1790c75?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80'
-            category='Aksesoris'
-            title='Jam Tangan Rolex'
-            price={500000000}
-          />
+          {products.map((product) => (
+            <Card
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              imgUrl={product.imgUrl}
+              category={product.category}
+              price={product.price}
+            />
+          ))}
         </div>
       </LayoutWrapper>
     </>
