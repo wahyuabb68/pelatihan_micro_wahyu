83d11198d8d221e86ef3c6d9c8682b8876132a82
unknown <wahyuabb3@gmail,com>
2025-12-12 14:20:35 +0700

    update

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://pelatihan-micro-wahyu-backend-27lstdmva.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
